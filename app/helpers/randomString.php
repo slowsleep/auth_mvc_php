6587def4d3369c75a6551d1ad655a36d7bfcaa83
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-07-18 21:57:45 +0300

    Add helper randomString

---
+++ app/helpers/randomString.php
@@ -0,0 +1,11 @@
+<?php
+
+function randomString($length) {
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
