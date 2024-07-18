d2238a4a50d358c9f58cce70100b02b4d0b4d9e7
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-07-18 22:20:35 +0300

    Add index and bootstrap files

---
+++ app/bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+
+require_once 'app/config/config.php';
+require_once 'app/core/model.php';
+require_once 'app/core/controller.php';
+require_once 'app/core/view.php';
+require_once 'app/core/route.php';
+require_once 'app/core/db.php';
+require_once 'app/core/auth.php';
+require_once 'app/models/user_model.php';
+require_once 'app/models/vkuser_model.php';
+require_once 'app/helpers/sqliteDbManager.php';
+require_once 'app/helpers/randomString.php';
+
+$db_file = new SQLiteDbManager();
+$db_file->initDB();
+
+Route::start();
+
